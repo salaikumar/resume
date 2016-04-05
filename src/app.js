c44df6ebe32896f543dcb22e5000b64e914cc28e
Vijay Kumar <ind.vijayworldz@gmail.com>
2016-04-05 11:40:30 +0530

    Initial view of the resume and created sample data for resume contents

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Resume from './components/Resume';
+import resumeContext from './data/resume';
 
-ReactDOM.render(<Resume />,document.getElementById('resume-content'));
+ReactDOM.render(<Resume sourceData={resumeContext}/>,document.getElementById('resume-content'));
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,14 +3,39 @@ import Profile from './Profile';
 import ResumeContent from './ResumeContent';
 
 var Resume = React.createClass({
+    getInitialState(){
+        return{
+            resumeJson: null
+        }
+    },
+    componentDidMount(){
+        if(isMounted()){
+
+        }
+    },
     render(){
-        return(
-            <div>
-                <Profile />
-                <ResumeContent />
-            </div>
-        )
+        if(this.state.resumeJson){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <aside className="col-md-4">
+                            <div className="inner well">
+                                <Profile />
+                                {this.state.resumeJson.profile.summary}
+                            </div>
+                        </aside>
+                        <main className="col-md-8">
+                            <div className="inner well">
+                                <ResumeContent />
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            )
+        }else{
+            return <p>Loading...</p>
+        }
     }
-})
+});
 
 export default Resume;
